4e3dca1f2555f6d91df412764898a358eb503cbe
resufer <66846932+resufer@users.noreply.github.com>
2021-05-19 20:55:54 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-function App() {
-  return (
-    <div>app</div>
-  );
+import React from 'react';
+import Header from './Header/Header';
+import Lists from './Lists/Lists';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <Header />
+        <Lists />
+      </div>
+    )
+  }
 }
 
 export default App;
