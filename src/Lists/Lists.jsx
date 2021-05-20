b58a8c128d092156d1edfa41eb33fcaac4e4ebcb
resufer <66846932+resufer@users.noreply.github.com>
2021-05-20 23:27:30 +0300

    Create Lists.jsx

---
+++ src/Lists/Lists.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from './List/List';
+import { timeWork } from './timeWork';
+
+class Lists extends React.Component {
+  render() {
+    return (
+      <div> Lists </div.
+    )
+  }
+}
+
+export default Lists;
