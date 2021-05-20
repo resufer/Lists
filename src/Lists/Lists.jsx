a659e73f458c12a0b72f384361244cc0f426d135
resufer <66846932+resufer@users.noreply.github.com>
2021-05-20 23:29:24 +0300

    Update Lists.jsx

--- src/Lists/Lists.jsx
+++ src/Lists/Lists.jsx
@@ -3,9 +3,37 @@ import List from './List/List';
 import { timeWork } from './timeWork';
 
 class Lists extends React.Component {
+  state = {
+    data: null,
+    form: false
+  }
+
+  componentDidMount() {
+    let [data, form, saveNewData] = timeWork();
+    this.setState({ data, form });
+  }
+
   render() {
     return (
-      <div> Lists </div.
+      <div>
+        Lists
+         
+        {this.state.data && this.state.data.map(obj => {
+          return <List title={Object.keys(obj)[0]} obj={obj} />
+        })}
+        
+        {this.state.form && <div className="form">
+          <div className="common">
+            Common : <input type="text" />
+          </div>
+          <div className="theory">
+            Theory : <input type="text" />
+          </div>
+          <div className="practice">
+            Practice : <input type="text" />
+          </div>
+        </div>}
+      </div>
     )
   }
 }
