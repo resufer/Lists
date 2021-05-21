c5ca2647b76af0f67393a0d28c514611d6893b6b
resufer <66846932+resufer@users.noreply.github.com>
2021-05-21 21:18:29 +0300

    Update List.jsx

--- src/Lists/List/List.jsx
+++ src/Lists/List/List.jsx
@@ -3,8 +3,20 @@ import React from 'react';
 class List extends React.Component {
   render() {
     return (
-      <div> list </div>
-     )
+      <div className='list'>
+        <div className='title'>{this.props.title}</div>
+        <div className="leftSide">
+          {this.props.obj[this.props.title]
+            .map(o => Object.keys(o))
+            .map(el => <div className='side'>{el}</div>)}
+        </div>
+        <div className="rightSide">
+          {this.props.obj[this.props.title]
+            .map(o => Object.values(o))
+            .map(el => <div className='side'>{el}</div>)}
+        </div>
+      </div>
+    )
   }
 }
 
