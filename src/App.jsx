4d3d8f8955065f0486a5a67dc35eb9d32590c192
resufer <66846932+resufer@users.noreply.github.com>
2021-05-24 23:01:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './Header/Header';
 import Lists from './Lists/Lists';
 
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <Header />
         <Lists />
       </div>
     )
